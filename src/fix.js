effb2f3df4f51823d864ee73e65415894a73ee9c
Pavel Prichodko <prichodko.p@gmail.com>
2018-11-13 13:16:15 -0800

    feat: add basic fix functionality

--- src/fix.js
+++ src/fix.js
@@ -1,3 +1,36 @@
-export const fix = (dir = process.cwd(), opts) => {
-  // TODO: get desired configs to fix
+import inquirer from 'inquirer'
+import chalk from 'chalk'
+
+// overwrite dimmed default separator
+const separator = msg => {
+  const s = new inquirer.Separator()
+  s.line = msg
+  return s
+}
+
+export const fix = async (dir = process.cwd(), opts) => {
+  const { configs, place } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'place',
+      message: 'Where do you prefer placing config?',
+      choices: ['file', 'package.json'],
+    },
+    {
+      type: 'checkbox',
+      name: 'configs',
+      message: 'What do you want to fix?',
+      choices: [
+        separator(chalk.blue('— Out of date —')),
+        'husky',
+        separator(chalk.yellow('\n— Not matching to recommended —')),
+        'ESLint',
+        separator(chalk.red('\n— Missing —')),
+        'Prettier',
+      ],
+      pageSize: 10,
+    },
+  ])
+
+  // TODO: write file or property to package.json
 }
